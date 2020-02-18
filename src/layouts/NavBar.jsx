18ffecc60b9e22bae23c3d7d65511d883359cb64
aakbarkhan <aakuukhan@gmail.com>
2020-02-18 18:40:20 +0530

    nev bar name changes to Classes

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -34,7 +34,7 @@ const NavBar = () => (
     </StyledLink>
     <Nav>
       <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
+      <Link to="/blog">Classes</Link>
       <Link to="/about">About</Link>
     </Nav>
   </Headroom>
